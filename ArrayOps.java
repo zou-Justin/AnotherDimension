8c4139b53c00b80a9a39012ad636fd53d6b2c5b4
Justin Dylan <justin82049@gmail.com>
2020-10-22 13:21:15 -0400

    The sum function in ArrayOps

---
+++ ArrayOps.java
@@ -0,0 +1,10 @@
+public class ArrayOps{
+  public static int sum(int[]arr){
+    int sumArr = 0;
+    for (int i = 0; i < arr.length; i++){
+      sumArr += arr[i];
+    }
+    return sumArr;
+  }
+
+}
---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester{
+  public static void main(String[]args){
+    int[]w = {1,2,3};
+    System.out.println(ArrayOps.sum(w));
+  }
+}
